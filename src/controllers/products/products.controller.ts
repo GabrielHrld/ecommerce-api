6105493de1bd876826627bedc6d1ca9cad0b19e7
Gabi <g.federico.rodriguez@gmail.com>
2021-05-05 17:27:54 -0300

    products - categories services

--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -12,10 +12,15 @@ import {
   Res
 } from '@nestjs/common';
 import { Response } from "express";
+import { ProductsService } from "../../services/products/products.service";
+
 
 @Controller('products')
 export class ProductsController {
-  private mock: Array<object> = [];
+
+  constructor(
+    private productsService: ProductsService
+  ){}
 
   @Get('')
   getProducts(
@@ -23,7 +28,7 @@ export class ProductsController {
     @Query('offset') offset = 0,
     @Query('brand') brand: string,
   ) {
-    return this.mock;
+    return this.productsService.findAll()
   }
 
   @Get('filter')
@@ -33,31 +38,23 @@ export class ProductsController {
 
   @Get(':id')
   @HttpCode(HttpStatus.ACCEPTED)
-  getOne(@Res() response: Response, @Param('id') id: string) {
-    return {
-      message : `product ${id}`
-    };
+  getOne(@Param('id') id: string) {
+    return this.productsService.findOne(id);
   }
 
   @Post()
   create(@Body() payload: any) {
-    this.mock.push(payload);
+    return this.productsService.create(payload);
 
-    return payload;
   }
 
   @Put(':id')
   update(@Param('id') id: number, @Body() payload: any) {
-    return {
-      id,
-      payload,
-    };
+    return this.productsService.update(id, payload)
   }
 
   @Delete(':id')
   delete(@Param('id') id: number) {
-    return {
-      id,
-    };
+    return this.productsService.delete(id);
   }
 }
